b54351851e7e0df6b753eed076dcb658c5b39ba0
Kamil Fus <balzac91@gmail.com>
2016-05-15 18:21:40 +0200

    add uglifying

--- webpack.config.js
+++ webpack.config.js
@@ -6,11 +6,22 @@ module.exports = {
     path: './bin',
     filename: 'bundle.js'
   },
+  devtool: 'inline-source-map',
   module: {
     loaders: [{
       test: /\.jsx?$/,
       exclude: /node_modules/,
       loader: 'babel'
     }]
-  }
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      output: {
+        comments: false
+      }
+    })
+  ]
 };
